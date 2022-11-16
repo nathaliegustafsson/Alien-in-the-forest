2b77b4b5b8da23ca6d78042d9b3366aa621f29ba
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2022-11-16 19:00:11 +0100

    start of js file

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const textElement = document.getElementById('story-text');
+const alternativeButtons = document.getElementById('button-alternatives');
+
+
+/** Olika saker som varelsen plockar upp/tar med sig genom spelet */
+let state = {}
+
+
+/** Start-delen av spelet*/
+function startGame() {
+
+}
+
+function showTextNode(textNodeIndex) {
+    
+
+}
+
+
+function selectAlternative(option) {
+
+}
